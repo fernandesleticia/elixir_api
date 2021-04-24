240cc9a4a5e80ae41d6399b9cf77d4d3c1fb51ed
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-04-24 19:58:07 -0300

    add response render

--- lib/elixir_api_web/controllers/welcome_controller.ex
+++ lib/elixir_api_web/controllers/welcome_controller.ex
@@ -9,15 +9,13 @@ defmodule ElixirApiWeb.WelcomeController do
     |> handle_response(conn)
   end
 
-  defp handle_response({:ok, message}, conn) do
-    conn
-    |> put_status(:ok)
-    |> json(%{message: message})
-  end
+  defp handle_response({:ok, message}, conn), do: render_response(conn, message, :ok)
 
-  defp handle_response({:error, message}, conn) do
+  defp handle_response({:error, message}, conn), do: render_response(conn, message, :bad_request)
+
+  defp render_response(conn, message, status) do
     conn
-    |> put_status(:bad_request)
+    |> put_status(status)
     |> json(%{message: message})
   end
 end
