fd09af112333d1060958e17fb858e2223fa618f6
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-04-24 20:32:08 -0300

    using uuid on keys

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,10 @@ use Mix.Config
 config :elixir_api,
   ecto_repos: [ElixirApi.Repo]
 
+config :elixir_api, ElixirApi.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures the endpoint
 config :elixir_api, ElixirApiWeb.Endpoint,
   url: [host: "localhost"],
