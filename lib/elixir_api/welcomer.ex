d8fc29f826f316798050dd9a4918259f807f06e9
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-04-24 19:23:43 -0300

    create evaluate methods

--- lib/elixir_api/welcomer.ex
+++ lib/elixir_api/welcomer.ex
@@ -1,10 +1,22 @@
 defmodule ElixirApi.Welcomer do
-  def welcome(params) do
-    age = params["age"]
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
 
-    name =
-      params["name"]
+    name
       |> String.trim()
       |> String.downcase()
+      |> evaluate(age)
+  end
+
+  defp evaluate("let", 25) do
+    {:ok, "Hello again"}
+  end
+
+  defp evaluate(name, age) when age >= 20 do
+    {:ok, "Welcome #{name}"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass #{name}"}
   end
 end
