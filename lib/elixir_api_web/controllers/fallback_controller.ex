c79582e7f5040ffcd0ea9bbb0171228d3825c8f1
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-04-24 22:47:07 -0300

    add fallback controller

---
+++ lib/elixir_api_web/controllers/fallback_controller.ex
@@ -0,0 +1,12 @@
+defmodule ElixirApiWeb.FallbackController do
+  use ElixirApiWeb, :controller
+
+  alias ElixirApiWeb.ErrorView
+
+  def call(conn, {:error, %{result: result, status: status}}) do
+    conn
+    |> put_status(status)
+    |> put_view(ErrorView)
+    |> render("error.json", result: result)
+  end
+end
